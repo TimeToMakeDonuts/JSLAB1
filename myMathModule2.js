8095574300e54352aa32a19e85119d91ff38ae40
TimeToMakeDonuts <bartekobox@gmail.com>
2024-11-05 21:14:15 +0100

    lab6 cleanup

--- myMathModule2.js
+++
@@ -1,9 +0,0 @@
-class MathOperations {
-    add(a, b) { return a + b; }
-    subtract(a, b) { return a - b; }
-    multiply(a, b) { return a * b; }
-    divide(a, b) { return a / b; }
-    power(a, b) { return Math.pow(a, b); }
-}
-
-module.exports = MathOperations;
