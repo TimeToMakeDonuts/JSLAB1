f26e993f41a2ed1ca80ebe3e5238015fe88d4536
Bartłomiej Brzoza <93342798+TimeToMakeDonuts@users.noreply.github.com>
2024-12-15 21:05:05 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = {
+const nextConfig = {
     output: 'export', // Włączamy eksport statyczny
     experimental: {
         appDir: true, // Włączanie App Directory
     },
 };
+
+
+module.exports = nextConfig;
