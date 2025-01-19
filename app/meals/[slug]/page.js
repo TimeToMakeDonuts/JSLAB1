6f8198ede6eb5dc9ed8a69e93c5e09977ee5223f
Bartłomiej Brzoza <93342798+TimeToMakeDonuts@users.noreply.github.com>
2025-01-19 18:17:21 +0100

    Update page.js

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,38 +1,48 @@
-import { getMeal } from '@/lib/meals';
-import styles from './page.module.css';
+import Link from 'next/link';
+import classes from './page.module.css';
 import { Suspense } from 'react';
+import { getMeal } from '@/lib/meals';
 
-export default async function MealDetailPage({ params }) {
-  const { slug } = await params;
+async function MealDetail({ slug }) {
   const meal = await getMeal(slug);
 
   if (!meal) {
-    return <div>This meal does not exist!</div>;
+    return <div className={classes.error}>This meal doesn&apos;t exist! Wonderhoy...☆</div>;
   }
 
   return (
-      <div className={styles.container}>
-        {/* Sekcja nagłówka */}
-        <header className={styles.header}>
-          <div className={styles.imageWrapper}>
-            <img src={meal.image} alt={meal.title} className={styles.image} />
-          </div>
-          <div className={styles.headerText}>
-            <h1 className={styles.title}>{meal.title}</h1>
-            <p className={styles.summary}>{meal.summary}</p>
-            <p className={styles.creator}>
-              Created by: <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
-            </p>
-          </div>
-        </header>
+    <div className={classes.mealDetail}>
+      <header className={classes.header}>
+        <div className={classes.imageWrapper}>
+          <img src={meal.image} alt={meal.title} className={classes.image} />
+        </div>
+        <div className={classes.headerText}>
+          <h1 className={classes.title}>{meal.title}</h1>
+          <p className={classes.summary}>{meal.summary}</p>
+          <p className={classes.creator}>
+            Created by: <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+        </div>
+      </header>
 
-        {/* Sekcja instrukcji */}
-        <section className={styles.instructionsSection}>
-          <h2 className={styles.instructionsTitle}>Instructions</h2>
-          <pre className={styles.instructionsText}>{meal.instructions}</pre>
-        </section>
-      </div>
+      <section className={classes.instructionsSection}>
+        <h2 className={classes.instructionsTitle}>Instructions</h2>
+        <pre className={classes.instructionsText}>{meal.instructions}</pre>
+      </section>
+    </div>
   );
-
 }
 
+export default async function MealDetailPage({ params }) {
+  const { slug } = await params; // Await params to resolve the Promise
+
+  return (
+    <div>
+      <main className={classes.main}>
+        <Suspense fallback={<p className={classes.loading}>Fetching meal details...</p>}>
+          <MealDetail slug={slug} />
+        </Suspense>
+      </main>
+    </div>
+  );
+}
